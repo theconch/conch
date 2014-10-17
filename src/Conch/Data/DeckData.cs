c191ad9374d8664c1980fe1b16b10fb30689c459
Mark Rendle <mark@markrendle.net>
2014-10-17 11:21:39 +0100

    Moved Decks to wwwroot

--- src/Conch/Data/DeckData.cs
+++ src/Conch/Data/DeckData.cs
@@ -12,6 +12,7 @@ namespace Conch
     /// </summary>
     public class DeckData
     {
+        private static readonly char[] LeadingNumbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-' };
         private static readonly Regex NameExtractor = new Regex("^[0-9]*-(.*)\\.html$");
         private readonly string _deckName;
         private readonly string _htmlPathBase;
@@ -23,15 +24,16 @@ namespace Conch
             _environment = environment;
             _deckName = deckName;
             _htmlPathBase = "/decks/" + deckName;
-            _fileSystem = new PhysicalFileSystem(environment.ApplicationBasePath);
+            _fileSystem = new PhysicalFileSystem(environment.ApplicationBasePath + "\\wwwroot");
         }
 
         public IEnumerable<Slide> GetSlides()
         {
-            return FindSlides("Decks\\" + _deckName, _htmlPathBase);
+            var slides = FindSlides("Decks\\" + _deckName, _htmlPathBase).ToList();
+            return slides;
         }
 
-        private IEnumerable<Slide> FindSlides(string directory, string path)
+        private IEnumerable<Slide> FindSlides(string directory, string path, string prefix = "")
         {
             IEnumerable<IFileInfo> contents;
             if (_fileSystem.TryGetDirectoryContents(directory, out contents))
@@ -40,20 +42,25 @@ namespace Conch
                 {
                     if (info.IsDirectory)
                     {
-                        foreach (var sub in FindSlides(directory + "\\" + info.Name, path + "/" + info.Name))
+                        foreach (var sub in FindSlides(directory + "\\" + info.Name, path + "/" + info.Name, prefix + RemoveLeadingNumbers(info.Name) + "+"))
                         {
                             yield return sub;
                         }
                     }
                     else if (info.Name.EndsWith(".html", StringComparison.OrdinalIgnoreCase))
                     {
-                        yield return new Slide(ExtractName(info.Name))
+                        yield return new Slide(prefix + ExtractName(info.Name))
                         {
                             TemplateUrl = path + "/" + info.Name
                         };
                     }
                 }
             }
+        }
+
+        private static string RemoveLeadingNumbers(string name)
+        {
+            return name.TrimStart(LeadingNumbers);
         }
 
         private static string ExtractName(string fileName)
